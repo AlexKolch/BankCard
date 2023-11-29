a9ab1a7fdb4afe822c8b69b112ee23b5a38134df
Alexey Kolch <kolychenkov94@mail.ru>
2023-11-29 21:23:33 +0300

    Initial Commit

---
+++ BankCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankCard
+//
+//  Created by Алексей Колыченков on 29.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
